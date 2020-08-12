27234b987493a5d3b44d2832025c9f6b60433f2d
Raphael Toubiana <raphael.toubiana@gmail.com>
2020-08-12 15:52:31 +0200

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creation of 5 restaurants ..."
+restaurant1 = Restaurant.create(name: "Restaurant 1", address: "address1", category: "french")
+puts restaurant1
+restaurant2 = Restaurant.create(name: "Restaurant 2", address: "address2", category: "chinese")
+puts restaurant2
+restaurant3 = Restaurant.create(name: "Restaurant 3", address: "address3", category: "italian")
+puts restaurant3
+restaurant4 = Restaurant.create(name: "Restaurant 4", address: "address4", category: "french")
+puts restaurant4
+restaurant5 = Restaurant.create(name: "Restaurant 5", address: "address5", category: "french")
+puts restaurant5
+puts "Seed done"
